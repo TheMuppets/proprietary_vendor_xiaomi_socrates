caece1eb38d8cdb1d3c0d4c1908c6b330380b74c
WenHao2130 <wenhao2130@outlook.com>
2026-02-17 17:54:46 +0800

    socrates: Remove useless dolby configurations
    Change-Id: I10082e42ce0dc5d106ca742ec0798e2e14f4bc7b
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

--- socrates-vendor.mk
+++ socrates-vendor.mk
@@ -129,8 +129,6 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/socrates/proprietary/odm/etc/disp0/mdss_dsi_m11_42_02_0a_dsc_cmd/mi_gc_2.txt:$(TARGET_COPY_OUT_ODM)/etc/disp0/mdss_dsi_m11_42_02_0a_dsc_cmd/mi_gc_2.txt \
     vendor/xiaomi/socrates/proprietary/odm/etc/disp0/mdss_dsi_m11_42_02_0a_dsc_cmd/mi_gc_3.txt:$(TARGET_COPY_OUT_ODM)/etc/disp0/mdss_dsi_m11_42_02_0a_dsc_cmd/mi_gc_3.txt \
     vendor/xiaomi/socrates/proprietary/odm/etc/display/qdcm_calib_data_xiaomi_42_02_0a_cmd_mode_dsc_dsi_panel.json:$(TARGET_COPY_OUT_ODM)/etc/display/qdcm_calib_data_xiaomi_42_02_0a_cmd_mode_dsc_dsi_panel.json \
-    vendor/xiaomi/socrates/proprietary/odm/etc/dolby/dax-default.xml:$(TARGET_COPY_OUT_ODM)/etc/dolby/dax-default.xml \
-    vendor/xiaomi/socrates/proprietary/odm/etc/dolby_vision.cfg:$(TARGET_COPY_OUT_ODM)/etc/dolby_vision.cfg \
     vendor/xiaomi/socrates/proprietary/odm/etc/init/vendor.qti.camera.provider-service_64.rc:$(TARGET_COPY_OUT_ODM)/etc/init/vendor.qti.camera.provider-service_64.rc \
     vendor/xiaomi/socrates/proprietary/odm/etc/mdss_dsi_m11_42_02_0a_dsc_cmd_mi.xml:$(TARGET_COPY_OUT_ODM)/etc/mdss_dsi_m11_42_02_0a_dsc_cmd_mi.xml \
     vendor/xiaomi/socrates/proprietary/odm/etc/sensors/config/kailua_qsh_ble_0.json:$(TARGET_COPY_OUT_ODM)/etc/sensors/config/kailua_qsh_ble_0.json \
